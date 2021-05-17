d12bb3f7e23f3b8eaa0ae46de77b974fef971991
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 19:10:24 +0530

    Add files via upload

---
+++ for loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
