adb6b4ca8631819280cb7fd72636014d54d78184
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 19:53:21 +0530

    Add files via upload

---
+++ do while/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Do_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, res = 0, i = 1;
+            number = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                res = number * i;
+                Console.WriteLine("{0} X {1} = {2}", number, i, res);
+                i++;
+            } while (i <= 10);
+            Console.ReadLine();
+        }
+    }
+}
