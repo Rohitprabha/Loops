403396bd5f0bba2b59d2f0b66c60daff97ed22dd
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 19:50:34 +0530

    Add files via upload

---
+++ While Loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            long factorial = number;
+            while (number > 1)
+                factorial *= --number;
+            System.Console.WriteLine(factorial);
+            Console.ReadKey();
+        }
+    }
+}
