b1d86c57480baac4a85384cae501cbcae7947df7
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 19:55:37 +0530

    Add files via upload

---
+++ Nested Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nested_For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, n = 1;
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(n + "   ");
+                    n++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
